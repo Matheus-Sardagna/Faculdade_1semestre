7090f6a5a326ea298ce68974a97f95b45c703f27
Matheus-Sardagna <matheussardagna02@gmail.com>
2024-03-16 23:54:29 -0300

    Estudo

--- Calculadora/src/Interface.java
+++ Calculadora/src/Interface.java
@@ -30,52 +30,52 @@ public class Interface extends JFrame {
         setLayout(null);
         JButton jButton2 = new JButton();
         add(jButton2);
-        jButton2.setText("=");
+        jButton2.setText("√");
         jButton2.setFont(new Font("Arial",Font.HANGING_BASELINE, 40));
         jButton2.setForeground(new Color(0, 0, 0));
         jButton2.setBackground(new Color(90, 90, 138));
-        jButton2.setBounds(360,400,120,55);
+        jButton2.setBounds(360,220,120,55);
 
         setLayout(null);
         JButton jButton3 = new JButton();
         add(jButton3);
-        jButton3.setText("=");
+        jButton3.setText("%");
         jButton3.setFont(new Font("Arial",Font.HANGING_BASELINE, 40));
         jButton3.setForeground(new Color(0, 0, 0));
         jButton3.setBackground(new Color(90, 90, 138));
-        jButton3.setBounds(360,400,120,55);
+        jButton3.setBounds(4,160,120,55);
 
         setLayout(null);
         JButton jButton4 = new JButton();
         add(jButton4);
-        jButton4.setText("=");
+        jButton4.setText("x");
         jButton4.setFont(new Font("Arial",Font.HANGING_BASELINE, 40));
         jButton4.setForeground(new Color(0, 0, 0));
         jButton4.setBackground(new Color(90, 90, 138));
-        jButton4.setBounds(360,400,120,55);
+        jButton4.setBounds(360,340,120,55);
 
         setLayout(null);
         JButton jButton5 = new JButton();
         add(jButton5);
-        jButton5.setText("=");
+        jButton5.setText("÷");
         jButton5.setFont(new Font("Arial",Font.HANGING_BASELINE, 40));
         jButton5.setForeground(new Color(0, 0, 0));
         jButton5.setBackground(new Color(90, 90, 138));
-        jButton5.setBounds(360,400,120,55);
+        jButton5.setBounds(360,160,120,55);
 
         setLayout(null);
         JButton jButton6 = new JButton();
         add(jButton6);
-        jButton6.setText("=");
+        jButton6.setText("-");
         jButton6.setFont(new Font("Arial",Font.HANGING_BASELINE, 40));
         jButton6.setForeground(new Color(0, 0, 0));
         jButton6.setBackground(new Color(90, 90, 138));
-        jButton6.setBounds(360,400,120,55);
+        jButton6.setBounds(360,280,120,55);
 
         setLayout(null);
         JButton jButton7 = new JButton();
         add(jButton7);
-        jButton7.setText("=");
+        jButton7.setText("+");
         jButton7.setFont(new Font("Arial",Font.HANGING_BASELINE, 40));
         jButton7.setForeground(new Color(0, 0, 0));
         jButton7.setBackground(new Color(90, 90, 138));
