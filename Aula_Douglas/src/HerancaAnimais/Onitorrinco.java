aeb42b10b4629bfa2841d7bb14418a1049aa35a6
Matheus-Sardagna <matheussardagna02@gmail.com>
2024-04-03 18:27:23 -0300

    SDAdal

---
+++ Aula_Douglas/src/HerancaAnimais/Onitorrinco.java
@@ -0,0 +1,9 @@
+package HerancaAnimais;
+
+public class Onitorrinco extends Mamifero {
+    private int qtdOvos;
+    public Onitorrinco(String nome,int qtdOvos) {
+        this.qtdOvos = qtdOvos;
+        super(nome,4);
+    }
+}
